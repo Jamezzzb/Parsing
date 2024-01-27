b1ce182f63668c12c1ece0dfea02f17c1b21c6fc
James Brown <jamesPersonal8888@gmail.com>
2024-01-27 01:43:57 -0500

    Use parameter packs instead of overload zip

--- Tests/ParsingTests/ParsingTests.swift
+++ Tests/ParsingTests/ParsingTests.swift
@@ -2,7 +2,17 @@ import XCTest
 @testable import Parsing
 
 final class ParsingTests: XCTestCase {
+    let testString = "Hello World"
     func testExample() throws {
+        let parser = Parser<Substring> { input in
+            guard !input.isEmpty else {
+                return nil
+            }
+            return Substring([input.removeFirst()])
+        }
+        let zipped = zip(parser, parser, parser, parser, parser, parser, parser)
+        let foo = zipped.run(testString).match.map({ $0 + $1 + $2 + $3 + $4 + $5 + $6 })
+        XCTAssertEqual(foo, "Hello W")
         // XCTest Documentation
         // https://developer.apple.com/documentation/xctest
 
